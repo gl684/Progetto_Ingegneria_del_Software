59cef082dfb8db12b2e565c28a293c6d6d4d858a
gl684 <gl.giangiacomi@gmail.com>
2024-12-16 20:45:33 +0100

    Add files via upload

---
+++ listaordini/controller/ControlloreListaOrdini.py
@@ -0,0 +1,20 @@
+from listaordini.model.ListaOrdini import ListaOrdini
+
+
+class ControlloreListaOrdini:
+    def __init__(self):
+        super(ControlloreListaOrdini, self).__init__()
+        # il suo attributo è il modello perche deve gestire il modello
+        self.model = ListaOrdini()
+
+    def get_lista_degli_ordini(self): #ho aggiunto return
+        return self.model.get_lista_ordini()
+
+    def get_ordine_da_indice(self, index):
+        return self.model.get_ordine_da_indice(index)
+
+    def get_lista_degli_ordini_filtrata(self, tipo_ordine):
+        return self.model.get_lista_ordini_filtrata(tipo_ordine)
+
+    def salva_dati(self):
+        self.model.salva_dati()  # Salva i dati quando necessario
