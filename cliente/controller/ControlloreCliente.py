2b18b6bf55c6ee37c19a58cbe664f6b21aaf880a
gl684 <gl.giangiacomi@gmail.com>
2024-12-17 14:50:13 +0100

    Add files via upload

---
+++ cliente/controller/ControlloreCliente.py
@@ -0,0 +1,15 @@
+from listaclienti.model.ListaClienti import ListaClienti
+
+
+class ControlloreCliente:
+    def __init__(self):
+        self.lista_clienti = ListaClienti()
+
+    def aggiungi_cliente(self, cliente):
+        self.lista_clienti.aggiungi_cliente(cliente)
+
+    def elimina_cliente(self, cliente_id):
+        self.lista_clienti.elimina_cliente(cliente_id)
+
+    def modifica_cliente(self, cliente_modificato):
+        self.lista_clienti.modifica_cliente(cliente_modificato)
---
+++ cliente/model/Cliente.py
@@ -0,0 +1,32 @@
+import uuid
+
+class Cliente:
+    def __init__(self, email, telefono, indirizzo, tipo_cliente, id=None, nome=None, cognome=None, cf=None, nome_azienda=None, partita_iva=None):
+        self.id = id if id else str(uuid.uuid4())  # Genera un UUID se l'ID non è fornito
+        self.email = email
+        self.telefono = telefono
+        self.indirizzo = indirizzo  # Campo comune spostato sopra
+        self.tipo_cliente = tipo_cliente  # "Privato" o "Azienda"
+
+        #Attributi per cliente privato
+        self.nome = nome
+        self.cognome = cognome
+        self.cf = cf
+
+        #Attributi per cliente azienda
+        self.nome_azienda = nome_azienda
+        self.partita_iva = partita_iva
+
+    def descrizione(self):
+        if self.tipo_cliente == "Privato":
+            return f"Cliente Privato: {self.nome} {self.cognome}, CF: {self.cf}, Indirizzo: {self.indirizzo}, Email: {self.email}, Telefono: {self.telefono}"
+        elif self.tipo_cliente == "Azienda":
+            return f"Cliente Azienda: {self.nome_azienda}, P.IVA: {self.partita_iva}, Indirizzo: {self.indirizzo}, Email: {self.email}, Telefono: {self.telefono}"
+        else:
+            return "Tipo di cliente sconosciuto."
+
+    def is_privato(self):
+        return self.tipo_cliente == "Privato"
+
+    def is_azienda(self):
+        return self.tipo_cliente == "Azienda"
